366bf946df6f923bfe603ed82ae8473e53cf996c
rahulrainaaa <rahulrainahome@live.com>
2018-05-01 13:44:08 +0530

    Initial Commit.

---
+++ app/src/main/java/com/product/bookmyvenue/BaseHandler.java
@@ -0,0 +1,7 @@
+package com.product.bookmyvenue;
+
+/**
+ * Base class for all the handler classes within the application.
+ */
+public abstract class BaseHandler {
+}
---
+++ app/src/main/java/com/product/bookmyvenue/UserProfileHandler.java
@@ -0,0 +1,12 @@
+package com.product.bookmyvenue;
+
+/**
+ * Handler class to handle the profile add, update, delete and query data.
+ */
+public class UserProfileHandler extends BaseHandler {
+
+    private static final String TAG = "UserProfileHandler";
+
+
+
+}
