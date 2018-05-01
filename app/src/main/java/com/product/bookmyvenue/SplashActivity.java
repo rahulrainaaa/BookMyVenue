8ca82ce9508daca761942afc936ae08f8ce378c1
rahulrainaaa <rahulrainahome@live.com>
2018-05-01 13:40:46 +0530

    Initial Commit.

--- app/src/main/java/com/product/bookmyvenue/SplashActivity.java
+++ app/src/main/java/com/product/bookmyvenue/SplashActivity.java
@@ -1,51 +1,153 @@
 package com.product.bookmyvenue;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Activity class for handling splash screen and {@link FirebaseAuth} phone signIn.<br/>
+ * 1. Check SMS Permissions.<br/>
+ * 2. On Login click, call initPhoneAuth() for OTP request.<br/>
+ * 3. On OTP receiving, call signInWithPhoneAuthCredential() to verify the OTP and login.<br/>
+ * 4. Call checkUserProfile() to get user profile (if it is existing user) and navigate to dashboard. If profile not found then (means: new user) navigate to create profile screen.
+ */
 public class SplashActivity extends AppCompatActivity {
+
+    public static final String TAG = "SplashActivity";
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        askForSMSPermission();
+        mAuth = FirebaseAuth.getInstance();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+
+            Toast.makeText(this, "Need to login.", Toast.LENGTH_SHORT).show();
+            // Need to login.
+        } else {
+
+            Toast.makeText(this, "Already logged in.", Toast.LENGTH_SHORT).show();
+            // Already login.
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+
+            initPhoneAuth("+917387802229");
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Method to enable the phone auth code.
+     *
+     * @param strMobile mobile number with country code as String.
+     */
+    private void initPhoneAuth(String strMobile) {
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(strMobile, 60, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+                Log.d(TAG, "Mobile verification successful.");
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+                Log.d(TAG, "Mobile verification failed.");
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
+    /**
+     * Method to verify the received OTP for mobile number login.
+     *
+     * @param phoneAuthCredential reference.
+     */
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(SplashActivity.this, "SignIn Successful", Toast.LENGTH_SHORT).show();
+                    checkUserProfile();
+
+                } else {
+
+                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+
+                        Toast.makeText(SplashActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    } else {
+
+                        Toast.makeText(SplashActivity.this, "Failed Exception", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Method to check if the user profile exists then navigate to dashboard.
+     * If new user login, then navigate to edit profile screen.
+     */
+    private void checkUserProfile() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    }
+
+    /**
+     * Method to prompt for SMS permission from user.
+     */
+    private void askForSMSPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(android.Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{android.Manifest.permission.RECEIVE_SMS}, 1);
+            }
         }
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/product/bookmyvenue/ToDo.java
@@ -0,0 +1,47 @@
+package com.product.bookmyvenue;
+
+public class ToDo {
+}
+
+/*
+
+
+{
+  "name": "",
+  "pic": "",
+  "address":"",
+  "mobile": "",
+  "phone",""
+  "rate": 5.555,
+  "rateCount":2,
+  "visit":12
+  "email": "",
+  "createdOn":"2018/05/28",
+  "lastLogin":"2018/06/15",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
