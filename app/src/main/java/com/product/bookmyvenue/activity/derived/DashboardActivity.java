cc1415fa296ee4e4ab5dc7cb66692be8b39279ae
rahulrainaaa <rahulrainahome@live.com>
2018-05-17 19:37:18 +0530

    Initial Commit.

--- app/src/main/java/com/product/bookmyvenue/activity/derived/DashboardActivity.java
+++ app/src/main/java/com/product/bookmyvenue/activity/derived/DashboardActivity.java
@@ -1,8 +1,14 @@
 package com.product.bookmyvenue.activity.derived;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.product.bookmyvenue.R;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -11,5 +17,45 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        StorageReference ref = storage.getReference().child("data");
+
+        String myString = "this is my example string.";
+
+        byte myBytes[] = myString.getBytes();
+
+        ref.putBytes(myBytes);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        StorageReference ref = storage.getReference().child("data");
+
+        ref.getBytes(1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+
+                String myString = bytes.toString();
+                Toast.makeText(DashboardActivity.this, myString, Toast.LENGTH_SHORT).show();
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+                Toast.makeText(DashboardActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
